15f5a16d59fb43007446e310b2cfe86c217afd76
Jack Hamilton <jack@vifive.ai>
2024-02-17 21:58:44 -0600

    sass version 1.0

---
+++ install.sh
@@ -0,0 +1,8 @@
+#Add sass to system path, copy files out of downloads or github
+cp sass /usr/local/bin
+
+#Copy templates and snippets
+
+#Exit
+echo "sass install complete, you may now delete the downloaded folder."
+echo
